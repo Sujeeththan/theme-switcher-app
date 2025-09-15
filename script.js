33baf3ca0dd7de930fe14886cd6b39573c18f294
sujeeththan <sujeethsujeeth487@gmail.com>
2025-09-15 23:43:51 +0530

    implement light,dark,,blueand forest theme toggle. store selected theme in localStorage.

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const themeSelector = document.getElementById("theme");
+
 const savedTheme = localStorage.getItem("theme");
 
+if (savedTheme) {
+  document.body.className = savedTheme;
+  themeSelector.value = savedTheme;
+} else {
+  document.body.className = "light";
+}
+
+themeSelector.addEventListener("change", function () {
+  document.body.className = this.value;
+  localStorage.setItem("theme", this.value);
+});
